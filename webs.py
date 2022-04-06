3154221813e306fa0738105c56b2ffc108554149
slackbot115 <viniccius.carvalho22@gmail.com>
2022-04-06 12:05:36 -0300

    first commit

---
+++ webs.py
@@ -0,0 +1,31 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+url = "https://www.cepea.esalq.usp.br/br/widgetproduto.js.php?"
+
+params = "id_indicador%5B%5D=54&id_indicador%5B%5D=91&id_indicador%5B%5D=leitep"
+
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.79 Safari/537.36'}
+
+page = requests.get(url+params, headers = headers)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+table_body = soup.find('tbody')
+linhas = table_body.find_all('tr')
+
+data, produto_tipo, preco = [], [], []
+for i in linhas:
+    children = i.findChildren("td")
+    data.append(children[0].text)
+    produto_tipo.append(children[1].text)
+    preco.append(children[2].text)
+
+df = pd.DataFrame({'Data': data, 'Produto': produto_tipo, 'Preço': preco})
+
+df.head()
+
+# df.to_json('tabela.json')
+df.to_excel('tabela.xlsx')
